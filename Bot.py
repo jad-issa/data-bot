5330042024601630cb4cc025afa65b2c779955e4
CharbelAD <abidahercharbel@gmail.com>
2020-03-20 02:05:52 +0200

    Added the /caps command and reply to an unknown command.

--- Bot.py
+++ Bot.py
@@ -14,11 +14,28 @@ def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+
+def unknown(update, context):
+    unknown_message = "Sorry, I didn't understand that command."
+    context.bot.send_message(chat_id=update.effective_chat.id, text=unknown_message)
+
+
 updater = Updater(token=API_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+caps_handler = CommandHandler('caps', caps)
+dispatcher.add_handler(caps_handler)
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 
