560e0d417a88c128cd4af8b52fb4edf43cc8e781
Jad Issa <jad.issa@protonmail.com>
2020-03-20 02:38:50 +0200

    Added basic inline keyboard interface

--- Bot.py
+++ Bot.py
@@ -1,8 +1,17 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+
 import logging
 
 with open("token.txt", "r") as file:
     API_TOKEN = file.readline()
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+
+logger = logging.getLogger(__name__)
 
 
 def start(update, context):
@@ -19,6 +28,22 @@ def caps(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 
+def ask(update, context):
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+                 InlineKeyboardButton("Option 2", callback_data='2')],
+
+                [InlineKeyboardButton("Option 3", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+
+def button(update, context):
+    query = update.callback_query
+    query.edit_message_text(text="Selected option: {}".format(query.data))
+    
+
 def unknown(update, context):
     unknown_message = "Sorry, I didn't understand that command."
     context.bot.send_message(chat_id=update.effective_chat.id, text=unknown_message)
@@ -33,13 +58,16 @@ dispatcher.add_handler(start_handler)
 caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
 
+ask_handler = CommandHandler('ask', ask)
+dispatcher.add_handler(ask_handler)
+
 unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
 
+callback_handler = CallbackQueryHandler(button)
+updater.dispatcher.add_handler(callback_handler)
+
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
 
 updater.start_polling()
